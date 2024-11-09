75e61671401e1dd292477a745ba0c25b18a2bf29
Dhruv Nair <dhruv14nair@gmail.com>
2024-11-09 19:32:00 +0530

    sixth commit

--- Project_01_3/settings.py
+++ Project_01_3/settings.py
@@ -127,6 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
